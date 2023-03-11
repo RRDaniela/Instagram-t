499159837b271253d3798294cbd4bcec838c5f4e
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2023-03-10 22:12:31 -0600

    Adding colors to be used globally.

---
+++ instagram_t/lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color appBar = Color(0xFFFF0B3954);
+  static const Color backColor = Color(0xFFFFBFD7EA);
+  static const Color headerColor = Color(0xFFFFFF6663);
+  static const Color bottomColor = Color(0xFFFFE0FF4F);
+  static const Color imageColor = Color(0xFFFFFEFFFE);
+}
