a0b966ad42a1d35dbf211aa7a54e35b47acfd44a
Barush Mendez <barush_mendez-3.0@hotmail.com>
2023-03-10 23:55:30 -0600

    ADDED: UI for login and signup was introduced, TODO: Fix TextFields

--- instagram_t/lib/main.dart
+++ instagram_t/lib/main.dart
@@ -11,13 +11,67 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.lightGreen),
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+          body: Column(children: [
+        Container(
+          margin: EdgeInsets.only(top: 70),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/stacked_logo.png'),
+
+            // ignore: prefer_const_constructors
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              child: const Text(
+                "Instagram't",
+                // ignore: prefer_const_constructors
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
         ),
-      ),
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+        ),
+        const Text("Let's get you started",
+            style: TextStyle(fontSize: 16, fontFamily: 'Montserrat-medium')),
+        Container(
+          margin: const EdgeInsets.only(top: 40),
+        ),
+        SizedBox(
+          width: 350,
+          height: 50,
+          child: TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                hintText: 'your_email@example.com',
+                fillColor: Color(0xC4B2BC)),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 40),
+        ),
+        SizedBox(
+          width: 350,
+          height: 50,
+          child: TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                hintText: '**********',
+                fillColor: Color(0xC4B2BC)),
+          ),
+        ),
+      ])),
     );
   }
 }
---
+++ instagram_t/lib/widgets/pebbles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Pebble extends StatelessWidget {
+  const Pebble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Figma Flutter Generator Rectangle9Widget - RECTANGLE
+    return Container(
+        width: 192,
+        height: 106,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(0),
+            topRight: Radius.circular(0),
+            bottomLeft: Radius.circular(160),
+            bottomRight: Radius.circular(160),
+          ),
+          color: Color.fromRGBO(11, 57, 84, 1),
+        ));
+  }
+}
