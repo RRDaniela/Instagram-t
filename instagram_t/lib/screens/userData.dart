2d5765ffeaa8c04b8f594dd32073fb7032e2e025
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2023-04-20 23:29:48 -0600

    ADDED: :sparkles: Added new page to add username'

--- instagram_t/lib/screens/userData.dart
+++ instagram_t/lib/screens/userData.dart
@@ -24,8 +24,82 @@ class _UserDataState extends State<UserData> {
   }
 
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('${currentUser?.email}'),
+    return Scaffold(
+      body: Container(
+        color: AppColors.background,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Enter your username',
+                    style: TextStyle(
+                        color: AppColors.onPrimaryContainer,
+                        fontSize: 25,
+                        fontFamily: 'Garamond',
+                        fontWeight: FontWeight.normal,
+                        decoration: TextDecoration.none)),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            width: 300,
+                            decoration: BoxDecoration(
+                                color: AppColors.background,
+                                border: Border.all(
+                                  color: AppColors.outline,
+                                ),
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 20),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Username',
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            )),
+                      ]),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 300,
+                    height: 50,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Next',
+                        style: TextStyle(
+                            fontFamily: 'Garamond',
+                            color: AppColors.onPrimary,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      style: TextButton.styleFrom(
+                          backgroundColor: AppColors.primary),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
